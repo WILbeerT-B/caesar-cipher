d0761de37780311bb32bce6043204ee7dc3b1e9b
WILbeerT-B <wilbertbundalian@gmail.com>
2023-10-20 06:27:08 -0700

    Delete inner IF (char > 90) condition,  and use UNTIL loop instead; Add comments

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -6,30 +6,31 @@ def caesar_cipher(string, shift)
   big_ord = (65..90)
   small_ord = (97..122)
 
+  #iterate through each char in string
   string.each_char do |char|
+
+    #assign ord value to char
+    char = char.ord
     
-    
-    char_ord = char.ord
-  
-    if big_ord.include?(char_ord)
-      char_ord += shift
-      if char_ord > big_ord.last
-        char_ord = big_ord.first + (shift - (big_ord.last - char.ord + 1))
-      end
-    
-    elsif small_ord.include?(char_ord)
-      char_ord += shift 
-      if char_ord > small_ord.last
-        char_ord = small_ord.first + (shift - (small_ord.last - char.ord + 1))
-      end
-    end
-    char = char_ord.chr
-    result += char
+    #check for upcase letters
+    if big_ord.include?(char)
+
+      #add shift value to char
+      char += shift
+
+      #reset char to (A) if it goes over (Z)
+      char = (big_ord.first + (char - big_ord.last) - 1) until char < (big_ord.last + 1)
+
     #binding.pry
+
+    #check for downcase letters
+    elsif small_ord.include?(char)
+      char += shift 
+      char = (small_ord.first + (char - small_ord.last) - 1) until char < (small_ord.last + 1)
+    end
+    result += char.chr
   end
   puts result
 end
 
-
-
-caesar_cipher("What a string!", 5)
+caesar_cipher("What a string!", 31)
